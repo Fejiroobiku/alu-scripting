bca0e95f200e0409aaa97906fa812968d011ced6
Fejiroobiku <o.obiku@alustudent.com>
2024-10-23 11:13:14 +0000

    H

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,45 +1,34 @@
 #!/usr/bin/python3
-"""
-This module queries the Reddit API and prints the titles of the first
-10 hot posts listed for a given subreddit.
-"""
-
+""" 2-recurse.py """
 import requests
 
 
-def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit to query.
-    
-    If the subreddit is invalid or not found, it prints 'None'.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+def recurse(subreddit, hot_list=[], after=None):
+    """List with titles of all hot Articles """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-                      "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
-    }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-    except requests.RequestException:
-        print(None)
-        return
-
-    # Check if the request was successful (status code 200)
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
+    params = {'after': after}
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
     if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        
-        if not data:
-            print(None)
-            return
-
-        # Print the titles of the first 10 posts
-        for i, post in enumerate(data[:10]):
-            print(post['data']['title'])
+        data = response.json().get('data')
+        if data is not None:
+            children = data.get('children')
+            if children is not None:
+                for child in children:
+                    hot_list.append(child.get('data').get('title'))
+                after = data.get('after')
+                if after is not None:
+                    return recurse(subreddit, hot_list, after)
+                else:
+                    return hot_list
+        else:
+            return hot_list
     else:
-        print(None)
-
-
+        return None
